d280e602b40a643ba6e64b41d5db67d7a76d88a4
BoMin <lbmleebm@gmail.com>
2025-10-19 14:36:23 +0900

    Initial commit

---
+++ LiveActivityPractice/LiveActivityPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityPracticeApp.swift
+//  LiveActivityPractice
+//
+//  Created by BoMin Lee on 10/19/25.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivityPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
